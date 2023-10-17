b02f2fdff0aa0f6a2f5d5910bf7d4284b0900ddd
KaliiMoon <132712673+KaliiMoon@users.noreply.github.com>
2023-10-17 08:18:41 -0500

    #29 only upload index.html if a content version has changed (#30)

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -25,6 +25,10 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
   content_type = "text/html"
 
   etag = filemd5(var.index_html_filepath)
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]  
+  }
 } 
 
  resource "aws_s3_object" "error_html" {
@@ -34,6 +38,9 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
   content_type = "text/html"
 
   etag = filemd5(var.error_html_filepath)
+  #lifecycle {
+  #  ignore_changes = [etag]
+  #}
 }  
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
@@ -56,4 +63,8 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
       }
     }
   })
+}
+
+resource "terraform_data" "content_version" {
+  input = var.content_version
 }
