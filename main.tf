7edbdfb077439dd3c2e1c2a3105ba27b57e04c63
KaliiMoon McClendon <csm154@gmail.com>
2023-10-30 22:12:47 +0000

    #41 finish implementation of terraform provider

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
   token = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
@@ -37,3 +37,15 @@ provider "terratowns" {
 #  content_version = var.content_version
 #  assets_path = var.assets_path
 #}
+
+resource "terratowns_home" "home" {
+  name = "How to play The Sims 4"
+  description = <<DESCRIPTION
+  The Sims 4 is a game from EA made in 2014 
+were you can create a world of Sims. 
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "42nk34.cloudfront.net"
+  town = "gamers-grotto"
+  content_version = 1
+}
